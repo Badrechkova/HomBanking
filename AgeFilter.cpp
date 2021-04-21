53339eb341c262464a3f3e15e3193ef00b20236e
Badr BENTALEB <75131546+Badrechkova@users.noreply.github.com>
2021-04-21 13:40:21 +0200

    Update AgeFilter.cpp

--- AgeFilter.cpp
+++ AgeFilter.cpp
@@ -42,8 +42,9 @@ int main (){
                     double age = stod(line.substr(16, 19));
                     if (Enc_RSA(a - 1) < Enc_RSA(age) and Enc_RSA(age) < Enc_RSA(b + 1))
                     {
-                        results << Dec_RSA(Enc_RSA(age)) << endl;
                         encResults << Enc_RSA(age) << endl;
+			results << Dec_RSA(Enc_RSA(age)) << endl;
+                        
                     }
 
                 }
