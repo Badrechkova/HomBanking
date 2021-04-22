4226c364499e7d881ba2be958512c0bdf333f249
Badr BENTALEB <75131546+Badrechkova@users.noreply.github.com>
2021-04-22 12:42:42 +0200

    Update RSAFunctions.h

--- RSAFunctions.h
+++ RSAFunctions.h
@@ -4,17 +4,54 @@
 #include<fstream>
 #include<string>
 #include<sstream>
+#include<random>
+
 
 //---------------------Function generates big numbers--------------------
 double big_random_number() {
-double x;
-x = rand() % 1000000000000000000 + 1000000000000000000;
-return x;
+    /* Seed */
+    std::random_device rd;
+
+    /* Random number generator */
+    std::default_random_engine generator(rd());
+
+    /* Distribution on which to apply the generator */
+    std::uniform_int_distribution<long long unsigned> distribution(0, 0xFFFFFFFFFFFFFFFF);
+
+    return  distribution(generator);
+}
+
+
+bool isPrime(int number)
+{
+    // 0 and 1 are not prime numbers
+    if (number == 0 || number == 1) {
+        return false;
+    }
+    else {
+        for (int a = 2; a < number / 2; a++) {
+            if (number % a == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+double big_prime(double big_num) {
+    double y;
+    if (isPrime(big_num) == true)
+    {
+        y = big_num;
+        return  y;
+    }
 }
 
 //------------------------Variables--------------------
-double p = big_random_number();
-double q = big_random_number();
+//double p = big_random_number();
+double p = big_prime(big_random_number());
+//double q = big_random_number(); 
+double q = big_prime(big_random_number());
 double n = p * q;
 double phi = (p - 1) * (q - 1);
 
@@ -33,7 +70,7 @@ int gcd(int a, int b) {
 //Generates e---------------------------------------------------
 double Gen_e(double phi) {
     double track;
-    double e = 11;
+    double e = 17;
     //for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
     while (e < phi) {
         track = gcd(e, phi);
@@ -55,8 +92,8 @@ double d = fmod(1 / e, phi);
 double Enc_RSA(double age) {
     double cryptAge;
     cryptAge = pow(age, e);
-   // std::cout << "encrypted message " << cryptAge << std::endl;
-    cryptAge=fmod(cryptAge,n);
+    // std::cout << "encrypted message " << cryptAge << std::endl;
+    cryptAge = fmod(cryptAge, n);
     //std::cout << "encrypted message modulo n: " << cryptAge << std::endl;
     return cryptAge;
 }
@@ -64,7 +101,14 @@ double Dec_RSA(double cipher) {
     double decryptCipher;
     decryptCipher = pow(cipher, d);
     //std::cout << "decrypted message " << decryptCipher << std::endl;
-    decryptCipher = fmod(decryptCipher,n);
-   // std::cout << "decrypted message modulo n: " << decryptCipher << std::endl;
+    decryptCipher = fmod(decryptCipher, n);
+    // std::cout << "decrypted message modulo n: " << decryptCipher << std::endl;
     return decryptCipher;
+}
+
+bool compareRSA(double l, double k) {
+    if (Enc_RSA(l) < Enc_RSA(k))
+    {
+        return 1;
+    }
 }
